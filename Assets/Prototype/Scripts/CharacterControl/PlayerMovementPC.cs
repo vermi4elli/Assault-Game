b484a18c498a689d6bb463455c1bb9a8640885e4
Никита Молодцов <nikita.molodtsov21@gmail.com>
2020-03-14 02:07:22 +0200

    Fixed the player movement via mouse

--- Assets/Prototype/Scripts/CharacterControl/PlayerMovementPC.cs
+++ Assets/Prototype/Scripts/CharacterControl/PlayerMovementPC.cs
@@ -10,20 +10,33 @@ public class PlayerMovementPC : MonoBehaviour
     
     //needed for movement of the character
     private Vector2 movement = Vector2.zero;
-    public float movSpeedKeys = 10f;
+    public float moveSpeedKeys = 10f;
 
     //needed for the rotation of the character
     private Vector2 mousePos = Vector2.zero;
     private Vector2 mousePressPos = Vector2.zero;
     private bool mouseLeftKeyPressed = false;
-    public float movSpeedMouse = 2f;
+    public float moveSpeedMouse = 2f;
+
+    //test value
+    private Vector2 screenCenter = new Vector2(Screen.width / 2, Screen.height / 2);
 
     void Update()
     {
+        //GetKeysInput();
+        GetMousePos();
+        GetMousePosAnyway();
+    }
+
+    private void GetMousePosAnyway()
+    {
+        mousePos = Input.mousePosition;
+    }
+
+    private void GetKeysInput()
+    {
         movement.x = Input.GetAxis("Horizontal");
         movement.y = Input.GetAxis("Vertical");
-
-        GetMousePos();
     }
 
     private void GetMousePos()
@@ -49,23 +62,28 @@ public class PlayerMovementPC : MonoBehaviour
     private void FixedUpdate()
     {
         //MovePlayerViaKeys();
-        MovePlayerViaMouse();
+        //RotatePlayerAnyway();
+
+        
         RotatePlayer();
+        MovePlayerViaMouse();
     }
 
     private void MovePlayerViaMouse()
     {
         if (mouseLeftKeyPressed)
         {
-            Vector2 direction = mousePos - mousePressPos;
-            direction.Normalize();
-            player.transform.Translate(new Vector3(direction.x, 0f, direction.y) * movSpeedMouse * Time.deltaTime);
-        }
-    }
+            //Vector2 direction = mousePos - mousePressPos;
+            //direction.Normalize();
+            //Vector3 moveDirection = new Vector3(direction.x, 0f, direction.y);
+            //Debug.Log(String.Format("x: {0}, y: {1}, z: {2}", moveDirection.x, moveDirection.y, moveDirection.z));
+            //player.transform.Translate(moveDirection * moveSpeedMouse * Time.deltaTime, Space.Self);
 
-    private void MovePlayerViaKeys()
-    {
-        player.transform.Translate(new Vector3(movement.x, 0f, movement.y) * movSpeedKeys * Time.deltaTime);
+            Debug.Log(String.Format("x: {0}, y: {1}, z: {2}", player.transform.forward.x, 
+                                                              player.transform.forward.y, 
+                                                              player.transform.forward.z));
+            player.transform.Translate(player.transform.forward * moveSpeedMouse * Time.deltaTime, Space.World);
+        }
     }
 
     private void RotatePlayer()
@@ -73,8 +91,22 @@ public class PlayerMovementPC : MonoBehaviour
         if (mouseLeftKeyPressed)
         {
             Vector2 direction = mousePos - mousePressPos;
-            float angle = Mathf.Atan2(direction.y, direction.x) * Mathf.Rad2Deg * -1;
-            player.transform.rotation = Quaternion.Euler(0f, angle, 0f);
+            direction.Normalize();
+            Vector3 lookDirection = new Vector3(direction.x, 0f, direction.y);
+            Quaternion rotation = Quaternion.LookRotation(lookDirection, Vector3.up);
+            player.transform.rotation = rotation;
         }
+    }
+
+    private void MovePlayerViaKeys()
+    {
+        player.transform.Translate(new Vector3(movement.x, 0f, movement.y) * moveSpeedKeys * Time.deltaTime);
+    }
+
+    private void RotatePlayerAnyway()
+    {
+        Vector3 lookDirection = player.GetComponent<Rigidbody>().velocity;
+        Quaternion rotation = Quaternion.LookRotation(lookDirection, Vector3.up);
+        player.transform.rotation = rotation;
     }
 }
