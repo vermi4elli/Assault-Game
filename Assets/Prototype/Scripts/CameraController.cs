2791b74dcc149213c15e9fe58e908e9ceb9bf73d
Никита Молодцов <nikita.molodtsov21@gmail.com>
2020-03-14 02:45:07 +0200

    Added the camera manager (moving camera after the player)

--- Assets/Prototype/Scripts/CameraController.cs
+++ Assets/Prototype/Scripts/CameraController.cs
@@ -4,11 +4,36 @@ using UnityEngine;
 
 public class CameraController : MonoBehaviour
 {
-    
+    //objects
+    public GameObject mainCamera;
+    public Transform player;
 
-    // Update is called once per frame
-    void Update()
+    //needed constants
+    public float verticalOffset = 20f;
+    public float followOffset = 10f;
+    public Vector3 followOffsetVector;
+    public float followSpeed = 5f;
+
+    //camera movement variables
+    private float verticalMovement = 0;
+    private Vector3 direction = Vector3.zero;
+
+    private void Start()
     {
-        
+        followOffsetVector = new Vector3(followOffset * -1, 0f, followOffset * -1);
+    }
+
+    private void Update()
+    {
+        verticalMovement = player.position.y + verticalOffset;
+
+        direction = player.position + followOffsetVector;
+        direction.y = verticalMovement;
+    }
+
+    private void FixedUpdate()
+    {
+        Vector3 moveDirection = direction - mainCamera.transform.position;
+        mainCamera.transform.Translate(moveDirection * followSpeed * Time.deltaTime, Space.World);
     }
 }
