a625f25efa625d5f80b57df3f7e1c66bc4861fdf
Никита Молодцов <nikita.molodtsov21@gmail.com>
2020-03-16 14:53:46 +0200

    Added WeaponManager

---
+++ Assets/WeaponManager.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WeaponManager : MonoBehaviour
+{
+    public Weapon weapon;
+
+    public struct Weapon
+    {
+        public string name;
+        public int ammo;
+        public bool selectiveFire;
+        public shootingType type;
+        public enum shootingType
+        {
+            semiAutomatic,
+            burst,
+            fullAutomatic            
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
