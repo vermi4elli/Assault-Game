f7ad397d9798537d6fd477582ccefd41953c189e
Никита Молодцов <nikita.molodtsov21@gmail.com>
2020-07-11 18:42:01 +0300

    INTERMEDIARY: Added the Enemy Control + Enemy Manager AND Modified material and enemy prefab

---
+++ Assets/Prototype/Scripts/EnemyControl/EnemyController.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class EnemyController : MonoBehaviour
+{
+    private Transform enemy;
+
+    void Start()
+    {
+        enemy = GetComponent<Transform>();
+    }
+
+    void Update()
+    {
+        
+    }
+}
---
+++ Assets/Prototype/Scripts/EnemyManager.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyManager : MonoBehaviour
+{
+    [SerializeField]
+    private GameObject enemyType;
+    [SerializeField]
+    private List<GameObject> enemies = new List<GameObject>();
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
