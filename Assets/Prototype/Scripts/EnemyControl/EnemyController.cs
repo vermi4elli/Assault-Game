851984e9f599fa4c6a64ea0c8486894077a65556
Никита Молодцов <nikita.molodtsov21@gmail.com>
2020-07-12 16:13:21 +0300

    Added the following player behaviour to the EnemyController

--- Assets/Prototype/Scripts/EnemyControl/EnemyController.cs
+++ Assets/Prototype/Scripts/EnemyControl/EnemyController.cs
@@ -5,15 +5,37 @@ using UnityEngine.AI;
 
 public class EnemyController : MonoBehaviour
 {
-    private Transform enemy;
+    public float lookRadius = 5f;
+    private float distanceToTarget;
+    
+    private NavMeshAgent agent;
+    private Transform target;
 
     void Start()
     {
-        enemy = GetComponent<Transform>();
+        target = PlayerManager.instance.player.transform;
+        agent = GetComponent<NavMeshAgent>();
     }
 
     void Update()
     {
-        
+        distanceToTarget = Vector3.Distance(target.position, transform.position);
+
+        if (distanceToTarget <= lookRadius)
+        {
+            agent.SetDestination(target.position);
+            
+            if (distanceToTarget <= agent.stoppingDistance)
+            {
+                FaceTarget();
+            }
+        }
+    }
+
+    private void FaceTarget()
+    {
+        Vector3 direction = (target.position - transform.position).normalized;
+        Quaternion lookRotation = Quaternion.LookRotation(new Vector3(direction.x, 0, direction.z));
+        transform.rotation = Quaternion.Slerp(transform.rotation, lookRotation, Time.deltaTime * 5f);
     }
 }
