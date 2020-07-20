88756002f72664f56f4c65ec333c3517d6387820
Никита Молодцов <nikita.molodtsov21@gmail.com>
2020-07-20 20:02:53 +0300

    Adding the EnemyManager

--- Assets/Prototype/Scripts/GameManager/EnemyManager.cs
+++ Assets/Prototype/Scripts/GameManager/EnemyManager.cs
@@ -1,5 +1,7 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
+using System.Threading;
 using UnityEngine;
 
 public class EnemyManager : MonoBehaviour
@@ -8,11 +10,23 @@ public class EnemyManager : MonoBehaviour
     private GameObject enemyType;
     [SerializeField]
     private List<GameObject> enemies = new List<GameObject>();
+    [SerializeField]
+    private int enemiesAmount;
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        for (int i = 0; i < enemiesAmount; i++)
+        {
+            GameObject spawnPoint = ChooseSpawnPoint();
+            GameObject enemyTemp = Instantiate(enemyType, spawnPoint.transform.position + Vector3.up, spawnPoint.transform.rotation);
+            enemies.Add(enemyTemp);
+        }
+    }
+
+    private GameObject ChooseSpawnPoint()
+    {
+        throw new NotImplementedException();
     }
 
     // Update is called once per frame
